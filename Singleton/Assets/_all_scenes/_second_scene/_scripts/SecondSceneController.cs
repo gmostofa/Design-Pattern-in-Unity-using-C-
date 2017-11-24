447a58f041ba1792bccbd6b34f679f085afbf81d
G_Mostofa <mostofart@gmail.com>
2017-11-24 19:52:40 +0600

    Singleton desing pattern added
    Singleton desing pattern added.

---
+++ Singleton/Assets/_all_scenes/_second_scene/_scripts/SecondSceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SecondSceneController : MonoBehaviour
+{
+
+    public Text textMessage;
+
+    void Start()
+    {
+        textMessage.text = GameController.Controller().Message();
+    }
+
+
+}
---
+++ Singleton/Assets/_module/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    static GameController gameController;
+
+    public static GameController Controller()
+    {
+        return gameController;
+    }
+    private void Awake()
+    {
+        if (gameController == null)
+        {
+            gameController = this;
+        }
+    }
+
+    public string Message()
+    {
+        return "This is From Game Controller. It is designed using Singleton design pattern.";
+    }
+
+    public void GoToSecondScene()
+    {
+        SceneManager.LoadScene("SecondScene");
+    }
+
+
+}
